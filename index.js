f021bc2634ff1d98b3aeade454b25cb9f2657da1
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-05-02 20:03:12 +0600

    data added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const cors = require('cors');
+const port = process.env.PORT || 3000;
+
+app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('Server is Running');
