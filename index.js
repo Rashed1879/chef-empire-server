ba86226987616e1ea0508ebf2a4f9673b7ca1ddc
Rashed1879 <rashedul15-1879@diu.edu.bd>
2023-05-02 20:12:33 +0600

    data load on server done

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 3000;
 
+const allChef = require('./data/chef.json');
+
 app.use(cors());
 
 app.get('/', (req, res) => {
 	res.send('Server is Running');
+});
+
+app.get('/allChef', (req, res) => {
+	res.send(allChef);
 });
 
 app.listen(port, () => {
